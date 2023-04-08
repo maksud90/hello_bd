647c0bd346f343c6de2d2b07fd631fcaf895c9b9
maksud90 <75708976+maksud90@users.noreply.github.com>
2023-04-08 13:01:12 -0400

    Add files via upload

---
+++ birth_year.sh
@@ -0,0 +1,13 @@
+#asking user age
+
+echo "What is your age?"
+
+read AGE
+
+#calculating birth year
+
+BIRTH_YEAR=$((2023-AGE))
+
+#printing user birth year
+
+echo "The user birth year is $BIRTH_YEAR"
